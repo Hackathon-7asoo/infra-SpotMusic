69212eb4293cce3c704b11360ce10eeaf66b75d5
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-02 12:01:20 -0300

    repos back e front

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,9 +1,18 @@
 # Create Artifact Registry Repository for Docker containers
-resource "google_artifact_registry_repository" "spotmusic-repo" {
+resource "google_artifact_registry_repository" "spotmusic-repo-back" {
   provider = google-beta
 
   location = "us-central1"
-  repository_id = "spotmusic"
-  description = "Imagens Docker to SpotMusic project"
+  repository_id = "spotmusic-back"
+  description = "Imagens Docker to SpotMusic BackEnd"
+  format = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "spotmusic-repo-front" {
+  provider = google-beta
+
+  location = "us-central1"
+  repository_id = "spotmusic-front"
+  description = "Imagens Docker to SpotMusic FrontEnd"
   format = "DOCKER"
 }
